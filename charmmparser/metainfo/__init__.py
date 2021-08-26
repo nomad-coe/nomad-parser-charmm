4ee8f2b6e0ca42c75a51ed3d1ef6e940bdef6531
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:10:05 +0200

    Adapted further metainfo changes

--- charmmparser/metainfo/__init__.py
+++ charmmparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import charmmparser.metainfo.charmm
+from nomad.metainfo import Environment
+
+from . import charmm
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, charmm.m_package)
